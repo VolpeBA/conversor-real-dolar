f62b8f71dd75688f474604fc094a7657460d12e9
Bruno Volpe <53006149+VolpeBA@users.noreply.github.com>
2023-09-29 12:53:02 -0300

    Start commit!

---
+++ app/src/main/java/com/volpe/conversorrealdolar/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.volpe.conversorrealdolar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.volpe.conversorrealdolar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener{
+            val real = binding.editReal.text.toString().toDouble()
+            val dolar = String.format("%.2f",real * 0.2)
+
+            binding.txtResultado.text = "$ $dolar"
+        }
+    }
+}
